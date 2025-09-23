ec400b75e0496577eb004dd87ef5b614c75876ef
Cristobal <cris.retamalv@duocuc.cl>
2025-09-23 11:15:49 -0300

    Commit HomeScreen

---
+++ app/src/main/java/com/example/urbanstyle/ui/theme/HomeScreen.kt
@@ -0,0 +1,22 @@
+package com.example.urbanstyle.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(){
+
+}
+
+
+
+@Composable
+@Preview(showBackground = true)
+fun HomeScreenPreview(){
+    HomeScreen()
+}
